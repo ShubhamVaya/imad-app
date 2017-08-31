515c1ccc5c1db85606f223062c8eb45cee586c4e
ShubhamVaya <svfuturestar31@gmail.com>
2017-08-31 12:35:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,53 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var page1 = {
+    title: 'page1',
+    content: `<p> Ab is pure page me jo bhi add karna ho kar sakta hai.. and aise kitne bhi pages banane ho bana sakta hai bus un sab pages ko server.js me jake link kar dena.</p>
+        <pre> Jo head tag me meta tag hai that is used for formatting and displaying the content of the web page based on the size of the device.</pre>
+        <p> U can even make changes in the inspect element option in the browser.</p>
+        <p>padding isliye diye hai because as we change the size of the browser apne text bhi us screen me fit ho sake with some padding in all the sides.. and yaad rakhna pre tag jaisa hai waisa hi rahega (ie it wont change its text when the browser is resized. </p>`
+};
+
+function createTemplate (data) {
+var title = data.title;
+var content = data.content;
+var htmltemp = `
+<html>
+<head>
+    <title>${title}</title>
+    <meta name="viewport" content="width-device-height, initial-scale=1" />
+    <style>
+        .c1{
+            margin: 0 auto;
+            font-family: Bradley Hand ITC;
+            color: purple;
+            padding-left: 20px;
+            padding-right: 20px;
+            padding-top: 40px;
+        }
+    </style>
+</head>
+<body class="c1">
+    <div>
+        <a href="/">Home</a>
+    </div>
+    <hr>
+    <div>
+        ${content}
+    </div>
+</body>
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/page1', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'page1.html'));
+   res.send(createTemplate(page1));
 });
 
 app.get('/page2', function (req, res) {
