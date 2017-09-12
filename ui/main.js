b501d38cf8629234df36958315e3ceef9f4e0d21
ShubhamVaya <svfuturestar31@gmail.com>
2017-09-12 19:17:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
 console.log('Loaded!');
+
+
+
+var button = document.getElementById('counter');
+button.Onclick = function() {
+    
+    //crating a request obj.
+    var request = new XMLHttpRequest();
+    
+    
+    //capture the response and store it in the variable
+    request.onreadystatechange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //Taking action
+            if(requestStatus === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    };
