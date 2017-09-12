9d5de3ae4aff4e9935a71696b7217eb2e8bf8ac5
ShubhamVaya <svfuturestar31@gmail.com>
2017-09-12 19:47:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,30 @@
+
+var button = document.getElementById('counter');
+
+button.onclick = function() {
     
+    //crating a request obj.
+    var request = new XMLHttpRequest();
+    
+    
+    //capture the response and store it in the variable
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Taking action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    //making request
+    request.open('GET', 'http://svfuturestar31.imad.hasura-app.io/counter', true);
+    request.send(null);
+    };
+
+
+
     var button = document.getElementById('counter');
 var counter = 0;
 
